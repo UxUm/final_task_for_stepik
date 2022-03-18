06a382565407707d51b5b83cf74a15187d9836ef
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-18 23:56:53 +0300

    eddit locators for test_product_page.py

--- locators.py
+++ locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators:
     REGISTRATION_EMAIL = (By.NAME, "registration-email")
     REGISTRATION_PASSWORD_1 = (By.NAME, "registration-password1")
     REGISTRATION_PASSWORD_2 = (By.NAME, "registration-password2")
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
