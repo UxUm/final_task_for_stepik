a2aa792dc2275f4e13ec0725491a94a8e31188fb
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-20 23:21:18 +0300

    add BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,22 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_EMAIL = (By.NAME, "login-username")
     LOGIN_PASSWORD = (By.NAME, "login-password")
     REGISTRATION_EMAIL = (By.NAME, "registration-email")
     REGISTRATION_PASSWORD_1 = (By.NAME, "registration-password1")
     REGISTRATION_PASSWORD_2 = (By.NAME, "registration-password2")
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
