56387a7d081a39c39b07fe4755a04318ac0964ab
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-20 19:55:14 +0300

    edit ProductPageLocators, need to fix PRODUCT_PRICE

--- locators.py
+++ locators.py
@@ -14,8 +14,8 @@ class LoginPageLocators:
 
 
 class ProductPageLocators:
-    ADD_TO_CART_BUTTON = (By.XPATH, '//*[@id="add_to_basket_form"]/button')
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
-    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
-    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
