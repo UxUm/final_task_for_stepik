564fc2b591f5c632debfb3fc87db28078d2c9784
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-21 15:15:37 +0300

    add mark need_review

--- test_product_page.py
+++ test_product_page.py
@@ -12,9 +12,10 @@ class TestUserAddToBasketFromProductPage():
         page = LoginPage(browser, link)
         page.open()
         page.register_new_user(email=str(time.time()) + "@fakemail.org", password="RAMZANKADIROV")
-        time.sleep(7)
+        time.sleep(10)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
@@ -30,6 +31,7 @@ class TestUserAddToBasketFromProductPage():
         page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -43,6 +45,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_message_basket_total()
 
 
+@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -57,6 +60,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasePage(browser, link)
@@ -83,8 +87,8 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_not_be_success_message_disappeared()
 
 
-def test_guest_cant_see_success_message(self):
+def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(self.browser, link)
+    page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
