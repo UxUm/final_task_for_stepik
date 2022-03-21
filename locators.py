e8deea7cbb8d761cddb5449455d4a21f229b2ea7
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-21 14:59:57 +0300

    add locators tfor registration

--- locators.py
+++ locators.py
@@ -1,16 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_EMAIL = (By.NAME, "login-username")
     LOGIN_PASSWORD = (By.NAME, "login-password")
     REGISTRATION_EMAIL = (By.NAME, "registration-email")
     REGISTRATION_PASSWORD_1 = (By.NAME, "registration-password1")
     REGISTRATION_PASSWORD_2 = (By.NAME, "registration-password2")
+    LOGIN_BUTTON = (By.ID, "login_link")
+    REGISTRATION_SUBMIT_BUTTON = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
@@ -19,3 +17,11 @@ class ProductPageLocators:
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
     PRODUCT_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.LINK_TEXT, 'View basket')
+    BASKET_TOTAL = (By.CSS_SELECTOR, 'class="total align-right"')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
