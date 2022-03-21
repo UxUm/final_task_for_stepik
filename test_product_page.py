d49b142c7d3f95bb1bdafaf6dd2da359c229b972
UxUm <83772866+UxUm@users.noreply.github.com>
2022-03-21 14:59:05 +0300

    Group test for user

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,33 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
+import time
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email=str(time.time()) + "@fakemail.org", password="RAMZANKADIROV")
+        time.sleep(7)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_added_to_cart()
+        page.should_be_message_about_adding()
+        page.should_be_message_basket_total()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
@@ -28,3 +55,36 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_basket_empty()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_added_to_cart()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_added_to_cart()
+    page.should_not_be_success_message_disappeared()
+
+
+def test_guest_cant_see_success_message(self):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(self.browser, link)
+    page.open()
+    page.should_not_be_success_message()
